d1f93b670c3d9af30d34e867a2f53847aca5d3df
rochelryu <core.irie@gmail.com>
2019-08-24 15:08:53 +0000

    Envoie de Deuxieme niveau 2

--- App.js
+++ App.js
@@ -25,14 +25,14 @@ io.on('connection', function (socket) {
         const age = (data.age)? data.age:"";
         const name = (data.name)? data.name:"";
         const commande = (data.commande)? data.commande:[];
-        console.log(data.ident,name,age,code,"Render-vous",data.address,typeof data.date,commande, data.autre, data.choice);
+        console.log(data.ident,name,age,code,"Render-vous",data.address, data.date,commande, data.autre, data.choice);
         console.log("/******BEGIN******/");
-        const service = await AdminQuerie.setCommande(data.ident,name,age,code,"Render-vous",data.address,data.date,commande, data.autre, data.choice);
+        const service = await AdminQuerie.setCommande(data.ident,name,age,code,"Render-vous",data.address,'',data.date, commande, data.autre, data.choice);
         if(service.etat){
             let info = {}
             const ville = await AdminQuerie.getAllMedecin();
             info.medecin = ville;
-            info.service = service.user
+            info.service = service.user;
             io.emit("newService", info)
         }
     })
