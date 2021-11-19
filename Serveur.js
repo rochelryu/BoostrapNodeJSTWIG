bf407844a20d471b7c1286979bbbc248e197d2a9
Rochelryu <core.irie@gmail.com>
2021-11-19 00:57:03 +0000

    update package

--- Serveur.js
+++ Serveur.js
@@ -16,48 +16,48 @@ const AdminRoute = require('./Routes/Admin/Index')
 const API = require('./Routes/API/Index')
 const ClientRoute = require('./Routes/Client/Index')
 class Serveur {
-     constructor(port){
-         this.port = port;
-         this.app = express();
-         this.setting();
-         this.middleware();
-         this.route();
-     }
-     getApp(){
-         return this.app;
-}
+    constructor(port) {
+        this.port = port;
+        this.app = express();
+        this.setting();
+        this.middleware();
+        this.route();
+    }
+    getApp() {
+        return this.app;
+    }
 
-     setting(){
-         this.app.use(express.static(__dirname+'/Views')); //On utilise ce paramettre pour dire a express ou se trouve nos fichier images | css | js. on les appels souvents des fichiers assets
-         this.app.set('views', __dirname+'/Views') //On utilise ce paramettre pour dire a express ou se trouve nos fichier page Template fontend
-         // This section is optional and used to configure twig.
-         this.app.set('view engine', 'twig');
-         this.app.set("twig options", {
-             allow_async: true, // Allow asynchronous compiling
-             strict_variables: false
-         });
-         this.app.use(bodyParser.json({ limit: '50mb', extended: true })); // pour formatter tout ce qui est envoyé en post en application/json
-         this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })) // pour crypter tout ce qui est post en application/x-www-form-urlencoded
-     }
+    setting() {
+        this.app.use(express.static(__dirname + '/Views')); //On utilise ce paramettre pour dire a express ou se trouve nos fichier images | css | js. on les appels souvents des fichiers assets
+        this.app.set('views', __dirname + '/Views') //On utilise ce paramettre pour dire a express ou se trouve nos fichier page Template fontend
+            // This section is optional and used to configure twig.
+        this.app.set('view engine', 'twig');
+        this.app.set("twig options", {
+            allow_async: true, // Allow asynchronous compiling
+            strict_variables: false
+        });
+        this.app.use(bodyParser.json({ limit: '50mb', extended: true })); // pour formatter tout ce qui est envoyé en post en application/json
+        this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })) // pour crypter tout ce qui est post en application/x-www-form-urlencoded
+    }
 
 
 
-     middleware(){
-         this.app.use(morgan("dev"));
-         this.app.use(session({
-             secret: config.secret,
-             resave: config.resave,
-             saveUninitialized: config.saveUninitialized,
-         }))
-     }
+    middleware() {
+        this.app.use(morgan("dev"));
+        this.app.use(session({
+            secret: config.secret,
+            resave: config.resave,
+            saveUninitialized: config.saveUninitialized,
+        }))
+    }
 
-     route(){
-         this.app.use('/ryu', AdminRoute);
-         this.app.use('/API/V1/focus/', API);
-         this.app.use('/', ClientRoute);
-     }
-    start(){
-        this.app.listen(this.port, ()=>{
+    route() {
+        //this.app.use('/ryu', AdminRoute);
+        this.app.use('/API/V1/focus/', API);
+        //this.app.use('/', ClientRoute);
+    }
+    start() {
+        this.app.listen(this.port, () => {
             console.log(`en cours sur ${this.port}`);
         })
     }
